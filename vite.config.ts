609f3e74e8e4f04ef930efab8582ddf7463bd239
Lunix-420 <davidhess336@gmx.de>
2025-08-06 17:30:41 +0200

    chore: Update dependencies and remove skeleton integration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		watch: {
+			usePolling: true, // Force polling if file changes aren't detected properly
+			interval: 100, // Polling interval (ms)
+			ignored: ['**/node_modules/**'] // What not to watch
+		}
+	}
 });
