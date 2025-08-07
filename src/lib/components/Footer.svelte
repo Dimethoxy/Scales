425bff9c976c952e3f9b6733c68e7fa8f20679da
Lunix-420 <davidhess336@gmx.de>
2025-08-07 23:46:41 +0200

    feat: Favicons

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,16 +2,17 @@
 </script>
 
 <footer
-	class="z-10 mt-auto flex w-full items-center justify-between bg-ctp-crust px-2 py-1 text-ctp-text"
+	class="z-10 mt-auto flex w-full items-center justify-between bg-ctp-crust px-2 text-ctp-text"
 >
 	<span class="text-sm">© 2025 Dimethoxy Audio. All Rights Reserved.</span>
 	<a
-		href="https://github.com/Dimethoxy"
+		href="https://github.com/Dimethoxy/Scales"
 		target="_blank"
 		rel="noopener noreferrer"
 		aria-label="GitHub"
-		class="transition-colors hover:text-ctp-lavender"
+		class=" transition-colors hover:text-ctp-lavender"
 	>
-		<i class="fa-brands fa-github text-2xl"></i>
+		<span> SOURCE </span>
+		<i class="fa-brands fa-github my-1 text-lg"></i>
 	</a>
 </footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 
